4fa0ab3edf02f0ce1987832d2188264493a030e9
tlzproject-aws <sarmaks+tlzproject-aws@amazon.com>
2022-01-18 22:55:40 -0800

    Updated s3 backend

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "3.44.0"
     }
   }
+  backend "s3" {
+    bucket = "circle-ci-backend-20220119064835623300000001"
+    key    = "terraform/webapp/terraform.tfstate"
+    region = "us-west-2"
+  }
   required_version = "> 0.14"
 }
 
