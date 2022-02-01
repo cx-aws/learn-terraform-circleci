5cbe746305fea8b77aee7f4dc6cb7f8b8cac4ce0
tlzproject-aws <sarmaks+tlzproject-aws@amazon.com>
2022-02-01 02:20:49 -0800

    Updated .gitignore and removed public-read acl on index.html

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_s3_bucket" "app" {
 }
 
 resource "aws_s3_bucket_object" "app" {
-  acl          = "public-read"
+  #acl          = "public-read"
   key          = "index.html"
   bucket       = aws_s3_bucket.app.id
   content      = file("./assets/index.html")
